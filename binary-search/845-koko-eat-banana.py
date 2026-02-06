ea90810fbb86509250d162be6cf27a4a9bf95871
vkk-rawat <>
2026-02-06 23:29:52 +0530

    refactor: clean up whitespace and comments in kokoEat function

--- binary-search/845-koko-eat-banana.py
+++ binary-search/845-koko-eat-banana.py
@@ -1,24 +1,26 @@
 "understand the problem first then solve and go for understanding"
 
+
 def kokoEat(arr, k):
     mx = max(arr)
-  
+
     for speed in range(1, mx + 1):
-      
+
         reqTime = 0
         for i in range(len(arr)):
-          
-            # time required to eat this pile 
+
+            # time required to eat this pile
             # of bananas at current speed
             reqTime += \
                 (arr[i] + speed - 1) // speed
-        
+
         # if total eating time at current speed
         # is smaller than given time
         if reqTime <= k:
             return speed
-  
+
     return mx
+
 
 if __name__ == "__main__":
     arr = [5, 10, 3]
